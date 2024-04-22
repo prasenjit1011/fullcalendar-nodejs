277691ac6089c9ade2082f047b356d43d93914af
Riyan <riyan04314@gmail.com>
2024-04-22 11:06:27 +0530

    Validation

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -10,6 +10,31 @@ exports.getEventData = (req, res, next)=>{
 
 exports.saveEventData = (req, res, next)=>{
 
+    if(typeof(req.body.eventTitle) == 'undefined'){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event title is mandatory!"}))
+    }
+    if(req.body.eventTitle == ''){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event title can\'t be empty!"}))
+    }
+
+    if(typeof(req.body.eventDate) == 'undefined'){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event date is mandatory!"}))
+    }
+
+    if(req.body.eventDate == ''){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event date can\'t be empty!"}))
+    }
+
+    var nowDate = new Date();
+    if(new Date(req.body.eventDate).getTime() < new Date(nowDate.toISOString().split('T')[0]).getTime()){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event date can\'t be past date!"}))
+    }
+
+    if(new Date(req.body.eventDate).getTime() > nowDate.getTime() + 7*24 * 60 * 60 * 1000){
+        return res.status(400).end(JSON.stringify({"status":400,msg:"Event date can\'t be more than 1 week!"}))
+    }
+
+    
     let eventList = JSON.parse(fs.readFileSync(fileName, 'utf8'));
     if(req.body.eventId != 0){
         eventList = eventList.filter((val)=>{ return val.id != req.body.eventId});
